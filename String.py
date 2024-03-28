5f59dfd102a12e469cf32bd5ed815ed81d9c0d62
SonalMittal-14 <sonalmittal140103@gmail.com>
2024-03-29 00:43:10 +0530

    Strings

---
+++ String.py
@@ -0,0 +1,35 @@
+text = "ice cream"
+print(text)
+print(text[0])
+
+# strings are imutable
+# hence they cannot be changed
+
+
+# suru or akhri ka index dedo jaha tak tak index doge use pehle tak ka vo string print karke dega 
+print(text[0:7])
+
+
+# given index se start karo or last tak jao
+print(text[2:])
+
+# zero index se start karooo and given index tak jao
+print(text[:6])
+
+
+# single quotes and double quotes are same 
+
+s1 = "sonal"
+s2 = "mittal"
+s3 = s1 + " " + s2
+print(s3)
+
+h =  76
+i = str(h)
+print(type(i))
+
+
+
+
+
+
