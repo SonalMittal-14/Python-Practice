ac8ea94b870f565ba285be9fb39667e5e4fd67af
SonalMittal-14 <sonalmittal140103@gmail.com>
2024-03-28 23:57:39 +0530

    Learning Number

---
+++ Number.py
@@ -0,0 +1,27 @@
+a = 12
+b = 3
+c = a ** b
+print(c)
+
+distance1 = 188
+distance2 = 247
+total_dis = distance1+distance2
+print(total_dis)
+mph = 65
+time = total_dis/mph
+print(time)
+
+round(time,2)
+
+d = 10
+e = 2
+f = 3
+g = (d+e)*f
+print(g)
+
+a = 6
+b = 5.7
+c = a-b
+print(c)
+
+
