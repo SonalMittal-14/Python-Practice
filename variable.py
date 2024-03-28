713f15f2f24e899c238bbcd4de1e7f3b81617217
SonalMittal-14 <sonalmittal140103@gmail.com>
2024-03-28 23:15:02 +0530

    first commit

---
+++ variable.py
@@ -0,0 +1,14 @@
+rent = 1220
+gas = 202.5
+groceries = 305.6
+print(rent)
+total = rent+gas+groceries
+print(total)
+
+rent = 1400
+
+item1 = "rent"
+item2 = "gas"
+item3 = "groceries"
+print("expense item:", item1,item2,item3)
+
