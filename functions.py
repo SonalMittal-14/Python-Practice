7fc0d285db942b8caa416f514d0cff86a1475837
SonalMittal-14 <sonalmittal140103@gmail.com>
2024-03-30 01:01:09 +0530

    Modules

--- functions.py
+++ functions.py
@@ -29,3 +29,8 @@ n = sum(2,3)
 print("total : ",n)
 
 
+def calculate_triangle_area(base,height):
+    return 1/2*(base*height)
+
+def calculate_square_area(length):
+    return length*length
---
+++ modules.py
@@ -0,0 +1,39 @@
+import math
+a = math.sqrt(16)
+print(a)
+
+b = math.pow(2,5)
+print(b)
+
+c = dir(math)
+print(c)
+
+d = math.pi
+print(d)
+
+e = math.log10(1087)
+print(e)
+
+f = math.floor(2.3)
+print(f)
+
+g = math.ceil(2.3)
+print(g)
+
+import calendar
+cal = calendar.month(2003,1)
+print(cal)
+
+
+cal1 = calendar.isleap(2003)
+print(cal1)
+
+cal2 = dir(calendar)
+print(cal2)
+
+
+import functions
+area = functions.calculate_square_area(5)
+print(area)
+
+
